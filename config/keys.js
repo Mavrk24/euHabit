818a3f878a4e30dc194a32c57fe1b3007e628f9b
KitRat <65643227+KitRat@users.noreply.github.com>
2021-07-06 23:37:53 +0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
-module.exports = {mongoURI: 
-    "mongodb+srv://A42wt3:m1a3q563@euhabit.d2cn5.mongodb.net/euHabit?retryWrites=true&w=majority",
+module.exports = {mongoURI: ,
     secretOrKey: "secret"
 };
