ca4e4fb20ea54c08cfac4f16d1302833b09a5d5d
KitRat <65643227+KitRat@users.noreply.github.com>
2021-07-06 23:45:31 +0700

    Delete keys.js

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # euHabit
+
+Office hour: 20.30-21.30 h
+
+## Split feature:
+   - Login
+   - Screening**
+   - Timer
+   - Match intervention 
+   - Demographic collection
+
+### feature Screening
+  * Login & Register (T: ~~debug~~ done, **M,N: In review**)
+  * Welcome page (N: in progress)
+  * **Data entry** (M,N)
+  * **Algorithm** (T)
+     (return result, database)
+  * Result (M)
+ ```
+    from DataentryAPI import NDI, ROSA
+    if NDI >40 && ROSA > cutoff:
+      return true
+    else:
+  ```
+  ## ของน็อบ แก้
+- [X] ทำ checkbox 
+   **Update 12.08**
+- [X] จัดหน้า username ดีๆ 
+   **Update 12.57**
+- [X] ทำ Terms and conditions
+   **Update 13.40**
+  ```  Login & Register กับ Welcome page ทำเสร็จแล้ว file upload ใน main และ Nob (Update 15.43)  ``` 
+- [ ] ทำ link ไปหน้าอื่น
+- [ ] *ทำ Navbar*
+- [ ] ทำ form
--- src/Profile.js
+++ src/Profile.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {React} from 'react';
 
 export default function Profile() {
@@ -10,3 +11,39 @@ export default function Profile() {
   </form>
   </div>)
 }
+=======
+import {React, useState, useEffect} from 'react';
+import axios from 'axios';
+import { Component } from 'react';
+
+export default class Content extends Component {
+    login = (e) => {
+    e.preventDefault();
+    axios
+    .post("/username", {
+      username: document.getElementById("username").value,
+      symptoms: document.getElementById("symptoms").value,
+})
+.then(function (response) {
+   console.log(response);
+   window.location.reload();
+});
+};
+  render(){
+  return(
+  <div className="login-wrapper"> 
+ <form onSubmit={this.login} method="post">
+    <p>
+       <label >X-value</label>
+       <input type="username" className="w3-input w3-border" id="username" name="username" />
+       <label >Y-value</label>
+       <input type="symptoms" className="w3-input w3-border" id="symptoms" name="symptoms" />
+   </p>
+   <p>
+      <input type="submit" className="w3-button w3-blue" value="Login" />
+   </p>
+</form>
+  </div>)
+  }
+  }
+>>>>>>> main
