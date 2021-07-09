b28b7e61a19b7fad63a09d74fb89aecdd8ccb06a
BonnisaP <pasinlarpanek@gmail.com>
2021-07-09 23:20:53 +0700

    เพิ่มภาพประกอบในตัวเลือกด้วย ของ ROSA

--- src/rosa.js
+++ src/rosa.js
@@ -8,64 +8,92 @@ export default class ROSA extends Component{
         return(
             <div> 
                 <h1 class="mx-5 pb-3" id="demographic-data"> 
-                    rapid office strain assessment (ROSA) แบบประเมินความเสี่ยงทางการยศาสตร์ด้วยวิธีโรซ่า
+                    rapid office strain assessment (ROSA) <br/>
+                    <p class="mt-2" id="survey"> แบบประเมินความเสี่ยงทางการยศาสตร์ด้วยวิธีโรซ่า </p>
                 </h1> 
                 
-                     
                 <p class="mx-5 p-3" id="instruction"> 
                     แบบสอบถามนี้ทำขึ้นเพื่อวิเคราะห์สถานีงานและท่าทางการนั่งทำงานกับคอมพิวเตอร์โดยประเมินจาก
                     1. ระดับของจอคอมพิวเตอร์และลักษณะการใช้โทรศัพท์สำนักงาน 
                     2. การใช้เมาส์และแป้นพิมพ์
-                    กรุณาทำเครื่องหมายลงในช่องเพียงหนึ่งช่อง
+                    กรุณาทำเครื่องหมายลงในช่อง "ทุกช่องที่ตรงกับคุณ"
                 </p>
 
 {/*คำถาม ROSA*/}
-                <p className="px-5"> การวิเคราะห์จอมอนิเตอร์และโทรศัพท์ </p>
+                <p className="px-5 mb-2"> การวิเคราะห์จอมอนิเตอร์และโทรศัพท์ </p>
+
+{/* เพิ่มภาพประกอบในตัวเลือกด้วย */}
+
                 <Form className="px-5" id="screening-form">
-                    <Form.Group className="mb-3 px-4">
-                        <Form.Label> 1. ความเจ็บปวด </Form.Label>
-                        {['radio'].map((type) => (
+{/*Q1 ข้อ 8*/}       <Form.Group className="mb-3 px-4">
+                        <Form.Label> 1. จอมอนิเตอร์ (Monitor) </Form.Label>
+                        {['checkbox'].map((type) => (
                             <div key={`default-${type}`} className="mb-3">
-                                <Form.Check 
+        {/* = 1 point */}       <Form.Check 
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ยังไม่มีความเจ็บปวดในขณะนี้"
+                                    name="rosa1"
+                                    label="จอห่างจากลำตัว 1 ช่วงความยาวแขน (40-75 ซม.) และจอสูงระดับดวงตา"
                                 />
-                                <Form.Check
+        {/* = 2 point */}       <Form.Check
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ปวดน้อยมากในขณะนี้"
+                                    name="rosa1"
+                                    label="จอต่ำเกินไป (ต่ำกว่า 30 องศา)"
                                 />
-                                <Form.Check
+        {/* = 3 point */}       <Form.Check
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ปวดปานกลางในขณะนี้"
+                                    name="rosa1"
+                                    label="จอสูงเกินไป (แหงนคอ)"
                                 />
-                                <Form.Check
+        {/* + 1 point */}       <Form.Check
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ปวดค่อนข้างรุนแรงในขณะนี้"
+                                    name="rosa1"
+                                    label="หมุนคอมากกว่า 30 องศา"
                                 />
-                                <Form.Check
+        {/* + 1 point */}       <Form.Check
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ปวดรุนแรงมากในขณะนี้"
+                                    name="rosa1"
+                                    label="มีแสงจ้าที่หน้าจอ"
                                 />
-                                <Form.Check
+        {/* + 1 point */}       <Form.Check
                                     type={type}
                                     id={`default-${type}`}
-                                    name="ndi1"
-                                    label="ปวดร้ายแรงที่สุดเท่าที่จะจินตนาการได้ในขณะนี้"
+                                    name="rosa1"
+                                    label="เอกสารไม่มีที่วาง"
+                                />
+        {/* + 1 point */}       <Form.Check
+                                    type={type}
+                                    id={`default-${type}`}
+                                    name="rosa1"
+                                    label="หน้าจอไกลเกินไป"
+                                />
+        {/* + 1 point */}       <Form.Check
+                                    type={type}
+                                    id={`default-${type}`}
+                                    name="rosa1"
+                                    label="ใช้งานต่อเนื่องมากกว่า 1 ชม. หรือมากกว่า 4 ชม./วัน"
+                                />
+        {/* + 0 point */}       <Form.Check
+                                    type={type}
+                                    id={`default-${type}`}
+                                    name="rosa1"
+                                    label="ใช้งานต่อเนื่อง 30 นาที - 1 ชม. หรือมากกว่า 1-4 ชม./วัน"
+                                />
+        {/* - 1 point */}       <Form.Check
+                                    type={type}
+                                    id={`default-${type}`}
+                                    name="rosa1"
+                                    label="ใช้งานต่อเนื่องน้อยกว่า 30 นาที หรือน้อยกว่า 1 ชม./วัน"
                                 />
                             </div>
                         ))}
                     </Form.Group>
                 </Form>
+
 
             </div>
         )
