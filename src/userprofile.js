2aee9ddf61b3fb2597a50a3a0a7657fd1ded5476
KiRa <kittikorn20009@gmail.com>
2021-09-02 14:36:59 +0700

    .

--- src/userprofile.js
+++ src/userprofile.js
@@ -57,62 +57,62 @@ export default class Profile extends Component{
             {/* Nav */}  
        
             <nav class="navbar navbar-light c">
-                <div class="container-fluid">
+            <div class="container-fluid">
             
-                   <a class="navbar-brand mb-0 h1" id="euHabitnavbar" href="#">            
-                    <svg width="30" height="24" class="d-inline-block align-text-top">
-                        <image href={LgeuHabit} height="30" width="24"/ >
-                    </svg>
-                    euHabit.
-                    </a>
+               <a class="navbar-brand mb-0 h1" id="euHabitnavbar" href="#">            
+                <svg width="30" height="24" class="d-inline-block align-text-top">
+                    <image href={LgeuHabit} height="30" width="24"/ >
+                </svg>
+                euHabit.
+                </a>
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/mainpage">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="/userprofile">Profile</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Prevention and Intervention
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="intervention">Ergonomics setting</a></li>
-                                    <li><a class="dropdown-item" href="stretching">Stretching</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/entry">Demographic Data</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/workplace">Workplace form</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/information">Information</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/" id="signout" type="submit" onClick={this.handleClick}>Sign Out</a>
-                            </li>
-                        </ul>
-                        <span class="navbar-text">
-                            Signed in as: <a href="#login">{this.state.user_data.username}</a> {/* ข้อมูลที่จะดึง done*/}
-                        </span>
-                    </div> 
-                </div>
-            </nav> {/* end Nav */}
-        
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/mainpage">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/userprofile">Profile</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Prevention and Intervention
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="intervention">Ergonomics setting</a></li>
+                                <li><a class="dropdown-item" href="stretching">Stretching</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/entry">Demographic Data</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/workplace">Workplace form</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/information">Information</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/" id="signout" type="submit" onClick={this.handleClick}>Sign Out</a>
+                        </li>
+                    </ul>
+                <span class="navbar-text">
+                    Signed in as: <a href="#login">{this.state.user_data.username}</a> {/* ข้อมูลที่จะดึง done*/}
+                </span>
+            </div> 
+                                   
+        </div>
+        </nav>
         {/* Profile */}
         
         <h1 id="profile">  Profile </h1> 
 
         <div class="d-flex flex-column content">
             <div>
-                <img src={rosa11} id="imgprofile" alt="rosa" />
+            <img src={rosa11} id="imgprofile" alt="rosa" />
             </div>
             <div class="d-flex flex-column">
                 <div class="d-flex justify-content-center">
